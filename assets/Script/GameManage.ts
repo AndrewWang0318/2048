e297c1873f402856cf44fbc9b18829827c9e10c3
WangNing <AndrewWang0318@outlook.com>
2024-08-16 18:22:16 +0800

    .

---
+++ assets/Script/GameManage.ts
@@ -0,0 +1,14 @@
+import { _decorator, Component, Node } from 'cc';
+const { ccclass, property } = _decorator;
+
+@ccclass('GameManage')
+export class GameManage extends Component {
+    start() {
+
+    }
+
+    update(deltaTime: number) {
+        
+    }
+}
+
